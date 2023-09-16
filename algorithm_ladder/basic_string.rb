f729a5956ed1c9f17ea1f9b6b49dc7f0322b3260
Leah Perri <perrileah2023@gmail.com>
2023-09-16 10:01:46 -0500

    reverses string

---
+++ algorithm_ladder/basic_string.rb
@@ -0,0 +1,24 @@
+# 1: Write a function that returns the reverse of a given string.
+
+# Input: “abcde”
+# Output: “edcba”
+
+# def reverse(string)
+#   index = 0
+#   result = ""
+#   while index < string.length
+#     result = string[index] + result
+#     index += 1
+#   end
+#   return result
+# end
+
+# p reverse("abcde")
+
+#2: Given a string, write a function that returns true if the “$” character is contained within the string or false if it is not.
+
+# Input: “i hate $ but i love money i know i know im crazy”
+# Output: true
+
+# Input: “abcdefghijklmnopqrstuvwxyz”
+# Output: false
