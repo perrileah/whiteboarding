cc9d87e92878b04b564003305bbf248f68c83ec3
Leah Perri <perrileah2023@gmail.com>
2023-10-23 12:02:20 -0500

    solves fizzbuzz

--- algorithm_ladder/basic_numbers.rb
+++ algorithm_ladder/basic_numbers.rb
@@ -1,18 +1,51 @@
-#Write a function that returns whether a given number is a prime number.
+#1 Write a function that returns whether a given number is a prime number.
 
-def is_prime(number)
+# def is_prime(number)
 
-index = 2
+# index = 2
 
-while index < number
-  p index
+# while index < number
+#   p index
 
-  return "NOT PRIME" if number % index == 0
-  index += 1
+#   return "NOT PRIME" if number % index == 0
+#   index += 1
+# end
+
+# return "PRIME"
+
+# end
+
+# p is_prime(12)
+
+#2 Write a function that prints out every number from 1 to N, with the following exceptions:
+
+# If the number is divisible by 3, print out "FIZZ".
+# If the number is divisible by 5, print out "BUZZ".
+# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+
+
+def fizz_buzz(number)
+
+  index = 1
+
+  while index <= number
+   
+    if (index % 3 == 0) && (index % 5 == 0)
+    puts "FIZZBUZZ"
+
+    elsif index % 3 == 0 
+      puts "FIZZ"
+
+    elsif index % 5 == 0
+      puts "BUZZ"
+
+    else
+      puts index
+    end
+
+    index += 1
+  end
+
 end
 
-return "PRIME"
-
-end
-
-p is_prime(12)
+p fizz_buzz(15)
