f4483a0fa2cfe0128540c5b4ebf1e610f05c0e4d
Leah Perri <perrileah2023@gmail.com>
2023-10-30 22:36:28 -0500

    more codewars fundamentals

--- code-wars/fundamentals.rb
+++ code-wars/fundamentals.rb
@@ -119,3 +119,56 @@
 # Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
 
+#7 -  Convert a number to a string
+
+# def number_to_string(num)
+
+#   p num.to_s
+ 
+# end
+
+# p number_to_string(8)
+
+#8 - Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+# Examples
+
+# Input: [1, 5.2, 4, 0, -1]
+# Output: 9.2
+
+# Input: []
+# Output: 0
+
+# Input: [-2.398]
+# Output: -2.398
+
+# Assumptions
+
+# You can assume that you are only given numbers.
+# You cannot assume the size of the array.
+# You can assume that you do get an array and if the array is empty, return 0.
+# What We're Testing
+
+# We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
+# Advanced users may find this extremely easy and can easily write this in one line.
+
+# def sum(numbers)
+
+#   index = 0 
+#   sum = 0
+
+#   while index < numbers.length
+#     sum = numbers[index] + sum
+#     index += 1
+
+#     if numbers == []
+#       return 0
+#     end
+
+#   end
+
+#   return sum
+
+# end
+
+# p sum([-2.398])
