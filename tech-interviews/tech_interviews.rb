cb4640b8297095394c577a88b389cc87a1073011
Leah Perri <perrileah2023@gmail.com>
2023-11-02 10:16:01 -0500

    reorganizes and solves interview problem

---
+++ tech-interviews/tech_interviews.rb
@@ -0,0 +1,29 @@
+# Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+
+#Input = "Leah"
+#Output = "134h"
+
+# def nerdify(string)
+
+# index = 0
+# result = ""
+
+# while index < string.length
+
+#   if string[index] == "L" || string[index] == "l"
+#     result << "1"
+#   elsif string[index] == "E" || string[index] == "e"
+#     result << "3"
+#   elsif string[index] == "A" || string[index] == "a"
+#     result << "4"
+#   else
+#     result << string[index]
+#   end
+#   index += 1
+# end
+
+# return result
+
+# end
+
+# p nerdify("Leah")
